5b00c8597f4148f323f880391622041995c3f206
madfree <matzefrei@gmail.com>
2020-08-24 21:31:22 +0200

    feat: Create data table

--- app/src/main/java/com/example/android/trackmysleepquality/database/SleepNight.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/database/SleepNight.kt
@@ -15,3 +15,15 @@
  */
 
 package com.example.android.trackmysleepquality.database
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "daily_sleep_quality_table")
+data class SleepNight(
+        @PrimaryKey(autoGenerate = true)
+        var nightId : Long = 0L,
+        var startTimeMilli : Long = System.currentTimeMillis(),
+        var endTimeMilli : Long = startTimeMilli,
+        var sleepQuality : Int = -1
+)
