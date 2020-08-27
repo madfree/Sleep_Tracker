cee8ee807878ae4e7b3f3364dbd7ae7de7495ac2
madfree <matzefrei@gmail.com>
2020-08-27 13:20:13 +0200

    feat: Implement ListAdapter and DiffUtil.ItemCallback to improve RecyclerView performance

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
@@ -81,7 +81,7 @@ class SleepTrackerFragment : Fragment() {
 
         sleepTrackerViewModel.nights.observe(viewLifecycleOwner, Observer {
             it?.let {
-                adapter.data = it
+                adapter.submitList(it)
             }
         })
 
